469d1840a7afdd2cb7df7068e2722656fdaa5057
barbok211 <barbarianz@hotmail.fr>
2016-04-13 16:55:14 +0200

    initial commit

---
+++ src/epsi/mtp/i4/engine/GameEngine.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epsi.mtp.i4.engine;
+
+/**
+ *
+ * @author mehdi <your.name at your.org>
+ */
+public class GameEngine {
+    public enum ANSWER { Yes, No, Nothing, MayBe, NotSure };
+}
---
+++ src/epsi/mtp/i4/models/Character.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epsi.mtp.i4.models;
+
+/**
+ *
+ * @author mehdi <your.name at your.org>
+ */
+public class Character {
+    private int id;
+    private String name;
+ 
+    public Character(int i, String n){
+        id = i;
+        name = n;
+    }
+}
